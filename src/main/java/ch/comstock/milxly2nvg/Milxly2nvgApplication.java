1a109ab8e2da1c1355ebff076a2a462b4b5f809f
Yvo von Känel <majortwip@comstock.ch>
2025-10-15 20:42:48 +0200

    init

---
+++ src/main/java/ch/comstock/milxly2nvg/Milxly2nvgApplication.java
@@ -0,0 +1,13 @@
+package ch.comstock.milxly2nvg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Milxly2nvgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Milxly2nvgApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ch/comstock/milxly2nvg/Milxly2nvgApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.comstock.milxly2nvg;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Milxly2nvgApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
